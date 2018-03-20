d00b77fee5b41bbd8b5f96dd860da6d6f41b4d67
Hunter Arthur Gabbard <hunter.gabbard@ldas-pcdev5.ligo-wa.caltech.edu>
2018-03-20 09:16:48 -0700

    First commit of main python and shell scripts

---
+++ rricard_gan/runGAN.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Use GPU:
+# May need flags
+#export THEANO_FLAGS="mode=FAST_RUN,device=cuda0,floatX=float32,gpuarray.preallocate=0.9"
+#export CPATH=$CPATH:/home/2136420/theanoenv/include
+#export LIBRARY_PATH=$LIBRARY_PATH:/home/2136420/theanoenv/lib
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/2136420/theanoenv/lib
+
+export CUDA_VISIBLE_DEVICES=0
+
+#####################
+# Running the network
+#####################
+
+# To run network call this file:
+# ./runGAN.sh
+# eg:
+# ./runCNN.sh 8 metricmass metricmass
+
+# train or generate?
+mode=train
+
+# directory for .txt waveform files are stored
+#data_file=/home/hunter.gabbard/Burst/rricard_gan/data/data_1000_samples.pkl
+data_file=/home/hunter.gabbard/Burst/rricard_gan/data/sineGuass_set/sineGauss100b10tau0d1/data.pkl
+
+# directory for output to be stored
+outdir=/home/hunter.gabbard/public_html/Burst/sine-gaussian_runs/100Hz_waveforms_runs
+
+# number of noise and GW waveform samples to train on
+n_samples=1000
+n_epochs=500
+g_lr=(0.5e-1)
+d_lr=(1e-6)
+
+# run script
+./nn.py --datafile=$data_file --n-samples=$n_samples --outdir=$outdir \
+--n_epochs=$n_epochs --g_lr=$g_lr --d_lr=$d_lr
