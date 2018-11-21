506a389df6559480c085f69c9dee74c688526cfd
hunter.gabbard <hunter.gabbard@ligo.org>
2018-11-21 11:34:55 -0800

    cleaned up get_lalinf_pars.py code

--- BBH_version/data/get_lalinf_pars.py
+++ BBH_version/data/get_lalinf_pars.py
@@ -1,3 +1,33 @@
+# Copyright (C) 2018  Hunter Gabbard, Chris Messenger
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#
+# =============================================================================
+#
+#                                   Preamble
+#
+# =============================================================================
+#
+
+'''
+This is a script which will take as input posteriors produced by lalinference and convert those to 
+chirp mass, inverse mass ratio, and compoenent values. The user gets to decide which of these values
+he or she would like to generate. This script assumes that you are rusing Python 2.7
+'''
+
 import h5py
 import pandas as pd
 from sympy import Eq, Symbol, solve
@@ -6,16 +36,15 @@ import pickle
 import numpy as np
 from sys import exit
 
-event_name = 'gw150914'
-event_time = '1126259462'
-posteriors = pd.read_hdf('/home/hunter.gabbard/parameter_estimation/john_bayesian_tutorial/injection_run_mass-time-varry_%s_srate-2048/lalinferencenest/posterior_samples/posterior_H1_%s-0.hdf5' % (event_name,event_time),'lalinference/lalinference_nest/posterior_samples')
+event_name = 'gw150914' # name of event to do PE on
+event_time = '1126259462' # time stamp of event to do PE on
+posteriors = pd.read_hdf('/home/hunter.gabbard/parameter_estimation/john_bayesian_tutorial/injection_run_mass-time-varry_%s_srate-2048/lalinferencenest/posterior_samples/posterior_H1_%s-0.hdf5' % (event_name,event_time),'lalinference/lalinference_nest/posterior_samples') # load all posteriors produced from lalinference
 
 
-# load lalinference chirp mass
-post_mc = posteriors['mc']
-# load lalinference q
-post_q = posteriors['q']
+post_mc = posteriors['mc'] # load lalinference chirp mass
+post_q = posteriors['q'] # load lalinference q
 
+# choose parameters you would like to convert posterior estimates to
 do_m1m2 = True
 do_mc_M = False
 do_mc_q = True
